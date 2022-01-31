ba2e5831ea34bd49f9f1772e375d1b656a91783c
Pelle Wessman <pelle@kodfabrik.se>
2022-01-31 20:30:53 +0100

    Fix tests + add types for help with spotting error

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,7 +1,7 @@
 const { ESLint } = require('eslint')
 const test = require('tape')
 
-test('load config in eslint to validate all rule syntax is correct', function (t) {
+test('load config in eslint to validate all rule syntax is correct', async t => {
   const cli = new ESLint({
     useEslintrc: false,
     overrideConfigFile: 'eslintrc.json'
@@ -9,11 +9,14 @@ test('load config in eslint to validate all rule syntax is correct', function (t
 
   const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
 
-  t.equal(cli.lintText(code).errorCount, 0)
+  const result = await cli.lintText(code)
+
+  t.equal(result.length, 1, 'Should return a single result item')
+  t.equal(result[0].errorCount, 0, 'The result item should show no errors')
   t.end()
 })
 
-test('space before an opening tag\'s closing bracket should not be allowed', t => {
+test('space before an opening tag\'s closing bracket should not be allowed', async t => {
   const cli = new ESLint({
     useEslintrc: false,
     overrideConfigFile: 'eslintrc.json'
@@ -52,15 +55,17 @@ test('space before an opening tag\'s closing bracket should not be allowed', t =
 
   const testPlansCount = Object.keys(shouldPass).length + Object.keys(shouldFail).length
 
-  t.plan(testPlansCount)
+  t.plan(testPlansCount * 2)
 
   for (const testCase in shouldPass) {
-    const { errorCount } = cli.lintText(shouldPass[testCase])
-    t.equal(errorCount, 0)
-  }
+    const result = await cli.lintText(shouldPass[testCase])
+    t.equal(result.length, 1, 'Should return a single result item')
+    t.equal(result[0].errorCount, 0, 'The result item should show no errors')
+    }
 
   for (const testCase in shouldFail) {
-    const { errorCount } = cli.lintText(shouldFail[testCase])
-    t.true(errorCount > 0)
-  }
+    const result = await cli.lintText(shouldFail[testCase])
+    t.equal(result.length, 1, 'Should return a single result item')
+    t.true(result[0].errorCount > 0, 'The result item should have errors')
+    }
 })
